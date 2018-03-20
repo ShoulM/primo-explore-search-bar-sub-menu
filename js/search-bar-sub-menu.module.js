8b02bf3da34ad562392fbb830aa8e17e69a261cf
Elliott Grieco <elliott.grieco@gmail.com>
2018-03-20 12:34:13 -0400

    Fixes unnamed scope in template

--- js/search-bar-sub-menu.module.js
+++ js/search-bar-sub-menu.module.js
@@ -5,7 +5,7 @@ angular
       const ctrl = this;
 
       ctrl.$onInit = () => {
-        this.items = searchBarSubMenuItems;
+        ctrl.items = searchBarSubMenuItems;
       };
       ctrl.goToUrl = (url) => { window.open(url, '_blank'); };
       ctrl.translate = (original) => {
@@ -14,10 +14,10 @@ angular
     },
     template: `<div class="layout-align-end-center layout-row flex search-bar-sub-menu">
                 <ul>
-                  <li ng-repeat="item in items">
-                  <button data-href="{{ translate(item.action) }}" aria-label="{{ translate(item.description) }}" ng-click="goToUrl(translate(item.action))" class="button-with-icon zero-margin md-button md-small {{item.cssClasses}}" type="button">
-                    <md-tooltip md-direction="bottom" md-delay="500">{{ translate(item.description) }}</md-tooltip><prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>
-                    <span class="search-bar-sub-menu-item" ng-class="(item.show_xs) ? '' : 'hide-xs'">{{ translate(item.name) }}</span>
+                  <li ng-repeat="item in $ctrl.items">
+                  <button data-href="{{ $ctrl.translate(item.action) }}" aria-label="{{ $ctrl.translate(item.description) }}" ng-click="goToUrl($ctrl.translate(item.action))" class="button-with-icon zero-margin md-button md-small {{item.cssClasses}}" type="button">
+                    <md-tooltip md-direction="bottom" md-delay="500">{{ $ctrl.translate(item.description) }}</md-tooltip><prm-icon style="z-index:1" icon-type="svg" svg-icon-set="{{item.icon.set}}" icon-definition="{{item.icon.icon}}"></prm-icon>
+                    <span class="search-bar-sub-menu-item" ng-class="(item.show_xs) ? '' : 'hide-xs'">{{ $ctrl.translate(item.name) }}</span>
                   </button>
                   </li>
                 </ul>
